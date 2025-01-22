c7e0aed2593a4dce660b9813b4adfc3c6df62c42
DoriaFabio <doriafabio2001@gmail.com>
2025-01-22 19:28:46 +0100

    add tags at the function show

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,13 +1,13 @@
 import connection from "../connection.js"
 // import { posts } from "../data/post.js";
-import CustomError from "../class/customError.js";  
+import CustomError from "../class/customError.js";
 
 // const { post } = require("../routers/posts.js");
 
 function index(req, res) {
   const sql = "SELECT * FROM blog_db.posts"
   connection.query(sql, (err, results) => {
-    if(err) return res.status(500).json({ error: "Database query failed" });
+    if (err) return res.status(500).json({ error: "Database query failed" });
     let data = results;
     const response = {
       totalCount: results.length,
@@ -21,20 +21,31 @@ function show(req, res) {
   const id = parseInt(req.params.id);
   const sql = "SELECT * FROM blog_db.posts WHERE `id` = ?";
   connection.query(sql, [id], (err, results) => {
-    if(err) return res.status(500).json({ error: "Database query failed"});
+    if (err) return res.status(500).json({ error: "Database query failed" });
     const item = results[0];
-    if(!item) {
+    if (!item) {
       return res.status(404).json({ error: "L'elemento non esiste" });
     }
-    res.json({ success: true, item });
+    const sqlTags = `SELECT tags.id, tags.label FROM blog_db.tags
+    JOIN post_tag ON post_tag.tag_id = tags.id
+    WHERE post_tag.post_id = ?`;
+    connection.query(sqlTags, [id], (err, results) => {
+      console.log(results);
+      if (err) return res.status(500).json({ error: "Database query failed" });
+
+      //aggiungo la proprietà ingredients all'oggetto pizza
+      item.tags = results;
+
+      res.json({ success: true, item });
+    });
   });
 }
 
 function store(req, res) {
   // res.send("Creazione nuovo post");
   let newid = 0;
-  for(let i = 0; i < posts.length; i++) {
-    if(posts[i].id > newid) {
+  for (let i = 0; i < posts.length; i++) {
+    if (posts[i].id > newid) {
       newid = posts[i].id
     }
   }
@@ -58,8 +69,8 @@ function update(req, res) {
   const item = posts.find((item) => item.id === id);
   if (item) {
     res.send(`Modifica integrale del post ${id}`);
-    for(key in item) {
-      if(key !== "id") {
+    for (key in item) {
+      if (key !== "id") {
         item[key] = req.body[key];
       }
     }
@@ -92,7 +103,7 @@ function destroy(req, res) {
   const index = posts.findIndex((item) => item.id === id);
   const sql = "DELETE * FROM blog_db.posts WHERE `id` = ?";
   connection.query(sql, [id], (err, results) => {
-    if(err) return res.status(500).json({ error: "Database query failed" });
+    if (err) return res.status(500).json({ error: "Database query failed" });
     res.sendStatus(204);
   });
 }
